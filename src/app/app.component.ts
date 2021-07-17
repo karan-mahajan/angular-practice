ee1c70f23078536e052306bcf43a3c31551afbb0
mahajan69 <karanmahajan321@gmail.com>
2021-07-17 18:07:35 +0530

    Added again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,5 +41,6 @@ export class AppComponent implements OnInit {
   countryEdited() {
     this.countries[this.editVal].conName = this.editCountry;
     this.editMode = false;
+    console.log('EDITED');
   }
 }
